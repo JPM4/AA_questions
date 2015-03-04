5022ff36632e3d4ba7540c5650ce8937fbe52c65
josh trotter <jtrot1000@gmail.com>
2015-03-03 18:00:39 -0800

    start implementing master save

---
+++ save.rb
@@ -0,0 +1,32 @@
+module Save
+  def save
+    return update unless @id.nil?
+
+    params = self.instance_variables.drop(1).map { |ivar| ivar.to_s[1..-1] }
+    params_eval = params.map { |ivar| eval(ivar.to_s) }
+
+    QuestionsDatabase.instance.execute(<<-SQL, *params_eval)
+      INSERT INTO
+        #{self.class.downcase} (#{params.join(", ")})
+      VALUES
+        (?#{", ?" * (params.count - 1) })
+    SQL
+
+    @id = QuestionsDatabase.instance.last_insert_row_id
+  end
+
+  def update
+    params = self.instance_variables.drop(1).map { |ivar| ivar.to_s[1..-1] }
+    params_eval = params.map { |ivar| eval(ivar.to_s) }
+
+    QuestionsDatabase.instance.execute(<<-SQL, *params_eval)
+      UPDATE
+        users (#{params.join(", ")})
+      VALUES
+        (?#{", ?" * (params.count - 1) })
+    SQL
+
+    @id = QuestionsDatabase.instance.last_insert_row_id
+  end
+
+end
